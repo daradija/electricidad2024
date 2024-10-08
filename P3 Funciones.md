42dcb98d004077ca1e0c84346b45ef88619f30b1
David Ragel Díaz-Jara <dragel@us.es>
2024-10-08 17:27:20 +0200

    initial

---
+++ P3 Funciones.md
@@ -0,0 +1,9 @@
+Leer antes la práctica.
+
+Usar / Crear
+Utilidad: 
+	Factor común.
+	La programación es el arte de mover código poco a poco.
+
+
+![[Pasted image 20241008172141.png]]
---
+++ p3_funciones/t1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+float area_triangulo(float base, float altura); // Prototipo
+
+int main (int argc, char *argv[]) {	
+	return 0;
+}
+
+
+// Definición de función
+float areas_triangulo(float base,float altura){
+	float area;
+	area=base*altura/2;
+	return area;
+}
